4b672127ae3f0a41288c5825b395472133dd3dbd
antonia <antonia.horincar@gmail.com>
2020-10-13 15:11:42 +0200

    Initial commit

---
+++ lib/keycloak_oauth.rb
@@ -0,0 +1,6 @@
+require "keycloak_oauth/version"
+
+module KeycloakOauth
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/keycloak_oauth/version.rb
@@ -0,0 +1,3 @@
+module KeycloakOauth
+  VERSION = "0.1.0"
+end
