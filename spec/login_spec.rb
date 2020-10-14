64cbd00b1ae549c3686cefc13f75785fafeb1bcc
antonia <antonia.horincar@gmail.com>
2020-10-14 11:00:45 +0200

    Renaming method

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 RSpec.describe KeycloakOauth::Views::Login do
   it 'generates button' do
-    expect(KeycloakOauth::Views::Login.login_button).to eq('<a href=http://TBA/auth/realms/TBA/protocol/openid-connect/auth?client_id=TBA&response_type=code>Login with Keycloak</a>')
+    expect(KeycloakOauth::Views::Login.button).to eq('<a href=http://TBA/auth/realms/TBA/protocol/openid-connect/auth?client_id=TBA&response_type=code>Login with Keycloak</a>')
   end
 end
