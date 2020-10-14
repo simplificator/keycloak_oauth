e4d35661e34fd2a72700664299420f1fd2fb1225
antonia <antonia.horincar@gmail.com>
2020-10-14 12:36:23 +0200

    Adding support for configuring connection

--- lib/keycloak_oauth.rb
+++ lib/keycloak_oauth.rb
@@ -1,7 +1,23 @@
 require 'keycloak_oauth/version'
-require 'keycloak_oauth/views/login'
+require 'keycloak_oauth/models/configuration'
+require 'keycloak_oauth/models/connection'
+require 'byebug'
 
 module KeycloakOauth
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.configure
+    yield(configuration) if block_given?
+  end
+
+  def self.configuration
+    Models::Configuration.instance
+  end
+
+  def self.connection
+    @connection ||= KeycloakOauth::Models::Connection.new(
+      auth_url: configuration.auth_url,
+      realm: configuration.realm,
+      client_id: configuration.client_id,
+      client_secret: configuration.client_secret
+    )
+  end
 end
---
+++ lib/keycloak_oauth/models/connection.rb
@@ -0,0 +1,21 @@
+module KeycloakOauth
+  module Models
+    class Connection
+      attr_accessor :auth_url, :realm, :client_id, :client_secret
+
+      DEFAULT_RESPONSE_TYPE = 'code'.freeze
+
+      def initialize(auth_url:, realm:, client_id:, client_secret:)
+        @auth_url = auth_url
+        @realm = realm
+        @client_id = client_id
+        @client_secret = client_secret
+      end
+
+      def authorization_endpoint(options)
+        endpoint = "#{auth_url}/realms/#{realm}/protocol/openid-connect/auth?client_id=#{client_id}"
+        endpoint += "&response_type=#{options[:response_code] || DEFAULT_RESPONSE_TYPE}"
+      end
+    end
+  end
+end
