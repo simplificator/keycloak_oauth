855787fd20b2a86fb93d430f6b303ebe2eccbc38
antonia <antonia.horincar@gmail.com>
2020-10-14 11:00:45 +0200

    Renaming method

--- lib/keycloak_oauth/views/login.rb
+++ lib/keycloak_oauth/views/login.rb
@@ -1,9 +1,7 @@
-# require 'actionview'
-
 module KeycloakOauth
   module Views
     class Login
-      def self.login_button
+      def self.button
         '<a href=http://TBA/auth/realms/TBA/protocol/openid-connect/auth?client_id=TBA&response_type=code>Login with Keycloak</a>'
       end
     end
