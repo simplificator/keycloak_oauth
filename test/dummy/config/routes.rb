a0560b89ae93a3954b83ba5a16dace7d9f3761c0
antonia <antonia.horincar@gmail.com>
2020-10-20 17:20:11 +0200

    Adding dummy routes

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   mount KeycloakOauth::Engine => "/keycloak_oauth"
+
+  get 'first_page' => 'pages#first_page'
+  get 'second_page' => 'pages#second_page'
+
+  root to: '/'
 end
