52049b46b5faa21f43f4eddad840d01c81cba91d
antonia <antonia.horincar@gmail.com>
2020-10-26 16:39:31 +0100

    Updating controller tests

--- spec/controllers/callbacks_controller_spec.rb
+++ spec/controllers/callbacks_controller_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
+require 'support/helpers/keycloak_responses'
 
 RSpec.describe KeycloakOauth::CallbacksController, type: :controller do
+  include Helpers::KeycloakResponses
+
   routes { KeycloakOauth::Engine.routes }
 
   describe "GET oauth2" do
    context 'when the host app overwrites after_sign_in_path' do
      it 'redirects to the specified path' do
+       stub_request(:post, 'http://domain/auth/realms/first_realm/protocol/openid-connect/token').
+         to_return(body: keycloak_tokens_request_body)
+
        get :oauth2
 
        expect(subject).to redirect_to('/second_page')
+       expect(session['access_token']).to eq(access_token)
+       expect(session['refresh_token']).to eq(refresh_token)
      end
    end
 
@@ -16,6 +24,9 @@ RSpec.describe KeycloakOauth::CallbacksController, type: :controller do
      subject { get :oauth2 }
 
      it 'redirects to root path' do
+       stub_request(:post, 'http://domain/auth/realms/first_realm/protocol/openid-connect/token').
+        to_return(body: keycloak_tokens_request_body)
+
        allow(described_class).to receive(:method_defined?).with(:after_sign_in_path).and_return(false)
        get :oauth2
 
