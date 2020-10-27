5967890fbc57dbb0cab0ffa9fd74e88c029e702b
antonia <antonia.horincar@gmail.com>
2020-10-27 13:14:16 +0100

    Allowing host app to implement user mapping

--- app/controllers/keycloak_oauth/callbacks_controller.rb
+++ app/controllers/keycloak_oauth/callbacks_controller.rb
@@ -10,6 +10,7 @@ module KeycloakOauth
         session: session
       )
       authentication_service.authenticate
+      map_authenticatable_if_implemented(session)
 
       redirect_to self.class.method_defined?(:after_sign_in_path) ? after_sign_in_path(request) : '/'
     end
@@ -18,6 +19,14 @@ module KeycloakOauth
 
     def authentication_params
       params.permit(:code)
+    end
+
+    def map_authenticatable_if_implemented(request)
+      if self.class.method_defined?(:map_authenticatable)
+        map_authenticatable(request)
+      else
+        raise NotImplementedError.new('User mapping must be handled by the host app. See README for more information.')
+      end
     end
   end
 end
--- test/dummy/app/controllers/concerns/keycloak_oauth_callbacks.rb
+++ test/dummy/app/controllers/concerns/keycloak_oauth_callbacks.rb
@@ -8,4 +8,10 @@ module KeycloakOauthCallbacks
   def after_sign_in_path(request)
     request.params[:show_first_page].present? ? first_page_path : second_page_path
   end
+
+  def map_authenticatable(_request)
+    service = KeycloakOauth::UserInfoRetrievalService.new(access_token: session[:access_token])
+    service.retrieve
+    session[:user_email_address] = service.user_information['email']
+  end
 end
