6e3991ae7743e3e3d6ac7e6cd59057e72a671c2e
antonia <antonia.horincar@gmail.com>
2020-10-28 10:08:56 +0100

    Moving tests to endpoints spec

---
+++ spec/endpoints_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe KeycloakOauth::Endpoints do
+  let(:auth_url) { 'http://domain/auth' }
+  let(:realm) { 'first_realm' }
+  let(:client_id) { 'a_client' }
+  let(:client_secret) { 'a_secret' }
+
+  subject { KeycloakOauth.connection }
+
+  describe '#authorization_endpoint' do
+    subject { KeycloakOauth.connection }
+
+    it 'returns scoped authorization_endpoint' do
+      expect(subject.authorization_endpoint).to eq('http://domain/auth/realms/first_realm/protocol/openid-connect/auth?client_id=a_client&response_type=code')
+    end
+
+    context 'when response_type is passed in' do
+      it 'returns scoped authorization_endpoint with custom response_type' do
+        endpoint = subject.authorization_endpoint(options: { response_type: 'token' })
+
+        expect(endpoint).to eq('http://domain/auth/realms/first_realm/protocol/openid-connect/auth?client_id=a_client&response_type=token')
+      end
+    end
+
+    context 'when redirect_uri is passed in' do
+      it 'returns scoped authorization_endpoint with custom redirect_uri' do
+        endpoint = subject.authorization_endpoint(options: { redirect_uri: 'http://example.com/oauth2' })
+
+        expect(endpoint).to eq('http://domain/auth/realms/first_realm/protocol/openid-connect/auth?client_id=a_client&response_type=code&redirect_uri=http://example.com/oauth2')
+      end
+    end
+  end
+
+  describe '#authentication_endpoint' do
+    subject { KeycloakOauth.connection }
+
+    it 'returns scoped authorization_endpoint' do
+      expect(subject.authentication_endpoint).to eq('http://domain/auth/realms/first_realm/protocol/openid-connect/token')
+    end
+  end
+
+  describe '#user_info_endpoint' do
+    subject { KeycloakOauth.connection }
+
+    it 'returns scoped user_info_endpoint' do
+      expect(subject.user_info_endpoint).to eq('http://domain/auth/realms/first_realm/protocol/openid-connect/userinfo')
+    end
+  end
+
+  describe '#logout_endpoint' do
+    it 'returns scoped user_info_endpoint' do
+      expect(subject.logout_endpoint).to eq('http://domain/auth/realms/first_realm/protocol/openid-connect/logout')
+    end
+  end
+end
