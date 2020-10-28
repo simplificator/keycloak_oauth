953360b0155e459956ef1ca6c772369a3fc015d9
antonia <antonia.horincar@gmail.com>
2020-10-28 10:02:29 +0100

    Adding support for handling more 2xx responses

--- app/services/keycloak_oauth/authorizable_service.rb
+++ app/services/keycloak_oauth/authorizable_service.rb
@@ -4,6 +4,7 @@ module KeycloakOauth
   class AuthorizableError < StandardError; end
 
   class AuthorizableService
+    HTTP_SUCCESS_CODES = [Net::HTTPOK, Net::HTTPNoContent]
     DEFAULT_CONTENT_TYPE = 'application/x-www-form-urlencoded'.freeze
     AUTHORIZATION_HEADER = 'Authorization'.freeze
 
@@ -17,14 +18,14 @@ module KeycloakOauth
     attr_accessor :access_token, :refresh_token
 
     def parsed_response(http_response)
-      response_hash = JSON.parse(http_response.body)
+      response = http_response.body.nil? ? http_response.body : JSON.parse(http_response.body)
 
-      return response_hash if http_response.code_type == Net::HTTPOK
+      return response if HTTP_SUCCESS_CODES.include?(http_response.code_type)
 
       # TODO: For now, we assume that the access token is always valid.
       # We do not yet handle the case where a refresh token is passed in and
       # used if the access token has expired.
-      raise KeycloakOauth::AuthorizableError.new(response_hash)
+      raise KeycloakOauth::AuthorizableError.new(response)
     end
   end
 end
